c569d70d3be0053b0808baab94a5178a7cd1658c
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-05-25 16:39:58 +0100

    Add Lab.03

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ let userGuess = function () {
 	let res = 0;
 
 	alert("Let's play a game. NOTE: Yes or No answers only");
-	let capitalCity = prompt("Is the capital city of London, London?");
+	let getName = prompt(
+		"First, let's get know each other. I am Munyaradzi. What is your name?"
+	);
+
+	let capitalCity = prompt(
+		`Is it true that I lived in Birmingham once upon a time?`
+	);
 
 	while (
 		capitalCity.toLowerCase() !== "yes" &&
@@ -13,15 +19,17 @@ let userGuess = function () {
 		capitalCity = prompt("Please enter a value that's either a Yes or No.");
 	}
 
-	if (capitalCity.toLowerCase() == "yes") {
+	if (capitalCity.toLowerCase() == "no") {
 		res += 1;
 		alert("Interesting, you are right.");
 	} else {
-		alert("You need Google in your life");
+		alert("Tough, you are wrong.");
 	}
 
 	// NOTE: second question
-	let spainCity = prompt("What is the capital of Spain?");
+	let spainCity = prompt(
+		"Is it true that I have 4 toes. It is said that I lost one when a I was a child"
+	);
 
 	while (
 		spainCity.toLowerCase() !== "yes" &&
@@ -33,14 +41,16 @@ let userGuess = function () {
 	if (spainCity.toLowerCase() == "no") {
 		res += 1;
 
-		alert("Fascinating, you might actually be smart");
+		alert(`Go ${getName}!!, Go ${getName}!! That is correct`);
 	} else {
-		alert("You need Google in your life");
+		alert(
+			"Actually I have six minus 3 divided by the square root of !!Stop reading you are wrong!! "
+		);
 	}
 
 	// NOTE: third question
 	let stepsCity = prompt(
-		"10 steps take 5 steps twice and 20 steps takes 2 times when 10 steps are a step. Is this statement true?"
+		`Is it true that I moved to England from South Africa`
 	);
 
 	while (
@@ -50,10 +60,10 @@ let userGuess = function () {
 		stepsCity = prompt("Please enter a value that's either a Yes or No.");
 	}
 
-	if (stepsCity.toLowerCase() == "yes") {
+	if (stepsCity.toLowerCase() == "no") {
 		res += 1;
 
-		alert("Well, this is truly a walk in the park for you.");
+		alert("Well well well, this is truly a walk in the park for you.");
 	} else {
 		alert(
 			"Breaking News!! We have a sunny day and a sprinkle of a wrong answer"
@@ -61,39 +71,64 @@ let userGuess = function () {
 	}
 
 	// NOTE: four question
-	let pillar = prompt(
-		"Four bottle of wine in a tree. Are there more than four bottles of wine in a tree?"
-	);
+	let pillar = prompt("Was Munyaradzi as a child a basketball lover?");
 
-	while (pillar.toLowerCase() !== "yes" || pillar.toLowerCase() !== "no") {
+	while (pillar.toLowerCase() !== "yes" && pillar.toLowerCase() !== "no") {
 		pillar = prompt("Please enter a value that's either a Yes or No.");
 	}
 
-	if (pillar.toLowerCase() == "no") {
+	if (pillar.toLowerCase() == "yes") {
 		res += 1;
-
-		alert("Those are truely good eyes, do you want to exchange?");
+		alert("Correct, I will make the next one tougher?");
 	} else {
-		alert("This question was a trick one, The brand four bottle is in a tree.");
-
-		//NOTE: five question
-		let what = prompt(
-			"Is today Wednesday if yesterday was tomorrow, today would be Friday?"
-		);
-
-		while (what.toLowerCase() !== "yes" || what.toLowerCase() !== "no") {
-			what = prompt("Please enter a value that's either a Yes or No.");
-		}
-
-		if (what.toLowerCase() == "yes") {
-			res += 1;
-			alert("Bravo!!, Game completed");
-		} else {
-			alert(
-				"Not this time champ, The answer is.. if x - 1 ==> x + 1 then x ==> x + 2, x + 2 = Friday, x = Wednesday"
-			);
-		}
+		alert("Not this time champ.");
 	}
+
+	//NOTE: five question
+	let what = prompt(
+		`I grew up with a friend who had the same as me, and we were best friends, Then I met ${getName} who happened to have the same name as you. Was my childhood bestfriends' name Munyaradzi? `
+	);
+
+	while (what.toLowerCase() !== "yes" || what.toLowerCase() !== "no") {
+		what = prompt("Please enter a value that's either a Yes or No.");
+	}
+
+	if (what.toLowerCase() == "yes") {
+		res += 1;
+		alert("Well my attempt to confuse you did not work. Great work");
+	} else {
+		alert(
+			`Imagine me dancing at the fact that you are wrong. Now know that you are wrong`
+		);
+	}
+
+	alert(`Well done, so far you have gotten ${res} out of 5. Let's change the pace a little bit.`)
+
+	//NOTE: six question
+	//
+
+	let ress = 2008;
+	let ansr = prompt('What year did I break my leg?');
+	let chcker = false;
+
+	for(let x = 4; x > 4; x--) {
+		if(ansr != ress) {
+			alert('Wrong answer, You have ' + x +  ' attempts left')
+			if (ansr < 2007) {
+				alert('Go highier!')
+			} else {
+				alert('Go lower!')
+			}
+
+		} else {
+			alert('Great job')
+		}
+	}			
+
+	if (!chcker) {
+		alert('The year I broke my leg was 2008. This happed while I was playing basketball in High School.')
+	}
+
 	return res;
 };
 
