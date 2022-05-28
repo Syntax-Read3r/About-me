0efe59f2c1bbe837201fc64b6aa70820ff7d5ffd
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-05-29 00:40:23 +0100

    Add nav bar and CSS

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+let res = 0;
+
 "use strict";
 
-//This function will ask the player for their name. 
+//NOTE: This function will ask the player for their name.
 let getUsername = function () {
-	let res = 0;
 
 	alert("Let's play a game. NOTE: Yes or No answers only");
 
 	let getName = prompt(
 		"First, let's get know each other. I am Munyaradzi. What is your name?"
 	);
-	return getName 
-}
+	return getName;
+};
 
-	//Question One
-	function questionOne() {
-
+//NOTE: Question One
+function questionOne() {
 	let answerOne = prompt(
 		`[Let's Begin] Is it true that I lived in Birmingham once upon a time?`
 	);
@@ -34,9 +34,8 @@ let getUsername = function () {
 	}
 }
 
-	//Question Two
-	function questionTwo() {
-
+//NOTE: Question Two
+function questionTwo() {
 	let answerTwo = prompt(
 		"Is it true that I have 4 toes. It is said that I lost one when a I was a child"
 	);
@@ -50,7 +49,6 @@ let getUsername = function () {
 
 	if (answerTwo.toLowerCase() == "no") {
 		res += 1;
-
 		alert(`Go ${getUsername()}!!, Go ${getUsername()}!! That is correct`);
 	} else {
 		alert(
@@ -59,9 +57,9 @@ let getUsername = function () {
 	}
 }
 
-	//Quesiton Three 
+//NOTE: Quesiton Three
 
-	function questionThree() {
+function questionThree() {
 	let answerThree = prompt(
 		`Is it true that I moved to England from South Africa`
 	);
@@ -72,7 +70,6 @@ let getUsername = function () {
 	) {
 		answerThree = prompt("Please enter a value that's either a Yes or No.");
 	}
-
 
 	if (answerThree.toLowerCase() == "no") {
 		res += 1;
@@ -85,13 +82,15 @@ let getUsername = function () {
 	}
 }
 
-	//Question Four
+//NOTE: Question Four
 
-	function questionFour() {
-
+function questionFour() {
 	let answerFour = prompt("Was Munyaradzi as a child a basketball lover?");
 
-	while (answerFour.toLowerCase() !== "yes" && answerFour.toLowerCase() !== "no") {
+	while (
+		answerFour.toLowerCase() !== "yes" &&
+		answerFour.toLowerCase() !== "no"
+	) {
 		answerFour = prompt("Please enter a value that's either a Yes or No.");
 	}
 
@@ -103,14 +102,16 @@ let getUsername = function () {
 	}
 }
 
-	//Question Five
-	function questionFive() {
-
+//NOTE: Question Five
+function questionFive() {
 	let answerFive = prompt(
 		`I grew up with a friend who had the same as me, and we were best friends, Then I met ${getName} who happened to have the same name as you. Was my childhood bestfriends' name Munyaradzi? `
 	);
 
-	while (answerFive.toLowerCase() !== "yes" && answerFive.toLowerCase() !== "no") {
+	while (
+		answerFive.toLowerCase() !== "yes" &&
+		answerFive.toLowerCase() !== "no"
+	) {
 		answerFive = prompt("Please enter a value that's either a Yes or No.");
 	}
 
@@ -122,48 +123,44 @@ let getUsername = function () {
 			`Imagine me dancing at the fact that you are wrong. Now know that you are wrong`
 		);
 	}
+}
 
-	alert(`Well done, so far you have gotten ${res} out of 5. Let's change the pace a little bit.`)
-
-
-	//Question Six
-	function questionSix() {
-		let chcker = false;
+//NOTE: Question Six
+function questionSix() {
+	let chcker = false;
 	let correctAnswer = 2008;
-	let answerSix = prompt('What year did I break my leg?');
+	alert(
+		`Well done, so far you have gotten ${res} out of 5. Let's change the pace a little bit.`
+	);
+	let answerSix = prompt("What year did I break my leg?");
 
-	for(let x = 3; x > 0; x--) {
-
-		if(answerSix != correctAnswer) {
-			alert('Wrong answer, You have ' + x +  ' attempts left')
+	for (let x = 3; x > 0; x--) {
+		if (answerSix != correctAnswer) {
+			alert("Wrong answer, You have " + x + " attempts left");
 			if (answerSix < 2007) {
-				alert('Go higher!')
+				alert("Go higher!");
 			} else {
-				alert('Go lower!')
+				alert("Go lower!");
 			}
-           answerSix = prompt('What year did I break my leg?')
+			answerSix = prompt("What year did I break my leg?");
 		} else {
-			res += 1
+			res += 1;
 			chcker = true;
-			alert('Great job')
+			alert("Great job");
 			break;
 		}
 	}
 	if (!chcker) {
-		alert('The year I broke my leg was 2008. This happened while I was playing basketball in High School.')
+		alert(
+			"The year I broke my leg was 2008. This happened while I was playing basketball in High School."
+		);
 	}
-	
-}			
+}
 
-	
-
-
-	//NOTE: Question Seven
-
-	alert('This is the final question. This question will have multiple correct answers.')
-
-	// let 
-
-	// return res;
-};
-
+//NOTE: Question Seven
+function questionSeven() {
+alert(
+	"This is the final question. This question will have multiple correct answers."
+);
+}
+;
